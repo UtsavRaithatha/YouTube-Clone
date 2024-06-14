a02087948447718a8043cb7c4456443c75bd327e
UtsavRaithatha <utsav.r@ahduni.edu.in>
2024-06-14 13:53:59 +0530

    maintenance page redirection issue solved

--- client/src/pages/Maintenance/Maintenance.jsx
+++ client/src/pages/Maintenance/Maintenance.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Maintenance.css";
 
 const Maintenance = () => {
+  // on reload redirect to "/"
+  const [redirected, setRedirected] = useState(false);
+
+  useEffect(() => {
+    if (!redirected) {
+      setTimeout(() => {
+        setRedirected(true);
+        window.location.href = "/";
+      }, 50000);
+    }
+  }, [redirected]);
+
   return (
     <div className="maintenance-container">
-      <h1>Maintenance in Progress</h1>
-      <p>We'll be back shortly. Thank you for your patience.</p>
+      <h1>Site Maintenance in Progress</h1>
+      <p className="maintenance-text">
+        Our website is currently undergoing scheduled maintenance from 1PM to
+        2PM IST. We apologize for any inconvenience caused.
+      </p>
+      <p className="maintenance-text">
+        Please check back later. Thank you for your patience.
+      </p>
     </div>
   );
 };
